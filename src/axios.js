aa87051cb60be5d3939b78dba15784e439e7b8b4
tungds232 <44081451+tungds232@users.noreply.github.com>
2019-12-26 19:08:58 +0700

    Update config for running kubernetes 2 nodes
    1 node backend
    1 node frontend

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL: "https://smart-store-7bb43.firebaseio.com"
-    baseURL: "http://171.244.37.150:8080"
+    baseURL: "http://192.168.0.123:30003"
 });
 
 export default instance;
